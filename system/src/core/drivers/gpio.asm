3fc413cb72fee94a00db9f850104e85cda699a1c
youssef <youssef@unique-host.unique-domain>
2024-07-02 18:19:31 +0100

    still writing the GPIO stupid driver

--- system/src/core/drivers/gpio.asm
+++ system/src/core/drivers/gpio.asm
@@ -6,6 +6,7 @@
 @ resets gpio port
 @ assumes r1, contains port to be reset: "A", "B", "C", "D", "E", "H"
 @-------------------------------------------------------------------
+  .global _gpio_port_reset
 _gpio_port_reset:
   LDR   r0, =RCC_BASE
   LDR   r1, [r0, #0x10] @ RCC_AHB1RST reg
@@ -52,11 +53,12 @@ __resetH:
   ORR   r1, r1, r2
   STR   r1, [r0, #0x10] @ RCC_AHB1RST reg
   BX    r14             @ return
-
+  .size _gpio_port_reset, .-_gpio_port_reset
 @-------------------------------------------------------------------
 @ enables gpio port's clock
 @ assumes r1, contains port to be enabled: "A", "B", "C", "D", "E", "H"
 @-------------------------------------------------------------------
+  .global _gpio_port_enable
 _gpio_port_enable:
   LDR   r0, =RCC_BASE
   LDR   r1, [r0, #0x30] @ RCC_AHB1ENR
@@ -103,7 +105,7 @@ __enableH:
   ORR   r1, r1, r2
   STR   r1, [r0, #0x30] @ RCC_AHB1ENR reg
   BX    r14             @ return
-
+  .size _gpio_port_enable, .-_gpio_port_enable
 
 @-------------------------------------------------------------------
 @ sets pin mode: (input/output/AF/Analog)
@@ -111,6 +113,7 @@ __enableH:
 @ assumes r2, contains pin-mode (I, O, F, A)=(input, output, alternate function, analog) 
 @ assumes r3, contains pin number
 @-------------------------------------------------------------------
+  .global _set_pinmode
 _set_pinmode:
   MULS  r3, r3, #2        @ Calculate the register bit offset for the pin number
   CMP   r1, #0x41
@@ -185,12 +188,14 @@ __set_pin_anlg:
   STR   r1, [r0]          @ GPIO_MODER reg
   BX    r14               @ return 
 
+  .size _set_pinmode, .-_set_pinmode
 @-------------------------------------------------------------------
 @ sets pin output type: (push-pull/open-drain)
 @ assumes r1, contains port: "A", "B", "C", "D", "E", "H"
 @ assumes r2, contains output type (P, O)=(push-pull, open-drain) 
 @ assumes r3, contains pin number
 @-------------------------------------------------------------------
+  .global _set_out_type
 _set_out_type:
   CMP   r1, #0x41
   ITE   EQ
@@ -230,7 +235,7 @@ __set_o_type:
   ORREQ r1, r1, r2        @ set bit to make it open-drain
   STR   r1, [r0, #0x04]   @ GPIOx_TYPER reg
   BX    r14
-
+  .size _set_out_type, .-_set_out_type
 @-------------------------------------------------------------------
 @ sets output speed of pin signal: (low-speed, medium-speed, high-speed, very-high-speed)
 @ assumes r1, contains port: "A", "B", "C", "D", "E", "H"
@@ -298,14 +303,75 @@ __set_o_vhigh_speed:
   ORR   r1, r1, r4        @ set bits at very high speed (11)
   STR   r1, [r0, #0x0C]   @ GPIOx_PUPDR reg
   BX    r14               @ return
+  .size _set_out_speed, .-_set_out_speed
+@-------------------------------------------------------------------
+@ set pullup-pulldown
+@ assumes r1, contains port to be reset: "A", "B", "C", "D", "E", "H"
+@ assumes r2, contains pin state: (U, D, N)=(pull-up, pull-down, None)
+@ assumes r3, contains pin number
+@-------------------------------------------------------------------
+_set_pull_updown:
+  MULS  r3, r3, #2        @ Calculate the register bit offset for the pin number
+  CMP   r1, #0x41
+  ITE   EQ
+  LDREQ r0, =GPIOA_BASE
+  BEQ   __set_pupd
+  CMP   r1, #0x42
+  ITE   EQ
+  LDREQ r0, =GPIOB_BASE
+  BEQ   __set_pupd
+  CMP   r1, #0x43
+  ITE   EQ
+  LDREQ r0, =GPIOC_BASE
+  BEQ   __set_pupd
+  CMP   r1, #0x44
+  ITE   EQ
+  LDREQ r0, =GPIOD_BASE
+  BEQ   __set_pupd
+  CMP   r1, #0x45
+  ITE   EQ
+  LDREQ r0, =GPIOE_BASE
+  BEQ   __set_pupd
+  CMP   r1, #0x48  
+  ITE   EQ
+  LDREQ r0, =GPIOH_BASE
+  BEQ   __set_pupd
+  BX    r14               @ return on failure
 
+__set_pupd:
+  LDR   r1, [r0, #0x0C]   @ GPIOx_PUPDR reg
+  MOV   r2, #0b11
+  LSL   r2, r2, r3
+  BIC   r1, r1, r2        @ clear bits
+
+  CMP   r2, #0x4E         @ no pull-up no pull-down
+  ITT   EQ
+  STREQ r1, [r0, #0x0C]   @ GPIOx_PUPDR reg
+  BXEQ  r14               @ return
+  
+  CMP   r2, #0x55         @ pull-up
+  ITTEE EQ
+  MOVEQ r2, #0b01
+  ORREQ r1, r1, r2        @ set bits
+  STREQ r1, [r0, #0x0C]   @ GPIOx_PUPDR reg
+  BXEQ  r14               @ return
+
+  CMP   r2, #0x44         @ pull-down
+  ITTEE EQ
+  MOVEQ r2, #0b10
+  ORREQ r1, r1, r2        @ set bits
+  STREQ r1, [r0, #0x0C]   @ GPIOx_PUPDR reg
+  BX    r14               @ return
+  .size _set_pull_updown, .-_set_pull_updown
 @-------------------------------------------------------------------
 @ reads pin input data
 @ assumes r1, contains port to be reset: "A", "B", "C", "D", "E", "H"
 @ assumes r2, contains pin number
 @-------------------------------------------------------------------
 _get_pin_value:
+
   
+  .size _get_pin_value, .-_get_pin_value
 
   .section .rodata.st_periphs, %progbits
   .extern RCC_BASE
