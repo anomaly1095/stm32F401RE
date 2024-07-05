093beee77fbeb12c0ee8dd1aee5feba32e1c6774
youssef <youssef@unique-host.unique-domain>
2024-07-05 14:24:16 +0100

    just finished the gpio driver i guess need to write exti interrupts and rcc next

--- drivers/helper.asm
+++ drivers/helper.asm
@@ -9,6 +9,12 @@
   .macro POP_R0_R3
     POP {r0-r3}
   .endm
+  .macro PUSH_R0_R44
+    PUSH {r0-r4}
+  .endm
+  .macro POP_R0_R4
+    POP {r0-r4}
+  .endm
   .macro PUSH_R0_R5
     PUSH {r0-r5}
   .endm
@@ -20,8 +26,18 @@
   .endm
   .macro POP_R0_R3
   .endm
+  .macro PUSH_R0_R4
+  .endm
+  .macro POP_R0_R4
+  .endm
   .macro PUSH_R0_R5
   .endm
   .macro POP_R0_R5
   .endm
 #endif
+
+.macro GPIO_CMP_LDR_PORT port_char, port_addr
+  CMP     r0, #\port_char
+  IT      EQ
+  LDREQ   r0, =\port_addr
+.endm
